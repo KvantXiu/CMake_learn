8e8df87e6478596989a60e35cb15bfd59e299951
KvantXiu <67580388+KvantXiu@users.noreply.github.com>
2020-07-01 17:06:28 +0800

    Add files via upload

---
+++ Tutorial.cxx
@@ -0,0 +1,8 @@
+ // 打印版本号
+ if (argc < 2) {
+    // report version
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  }
